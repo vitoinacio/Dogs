b9583403635d00328f51be2241f818b7ce2b315e
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-10-29 10:25:08 -0300

    build project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './style.modules/Header.module.css';
-import Dogs from '../Assets/dogs.svg?react'
+import Dogs from '../Assets/dogs.svg?react';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/" aria-label='Dogs - Home'><Dogs/></Link>
-        <Link className={styles.login} to="/Login">Login / Criar</Link>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/Login">
+          Login / Criar
+        </Link>
       </nav>
     </header>
-  )
+  );
 };
 
 export default Header;
