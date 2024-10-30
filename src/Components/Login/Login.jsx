3783fc5d0d16457aa4384688060e64525f254445
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-10-30 11:08:46 -0300

    use context mais elaborado adicao da funcao de logout e login automatico ao pegar o token do location

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LoginForm from './LoginForm'
-import LoginCreate from './LoginCreate.jsx'
-import LoginPasswordLost from './LoginPasswordLost.jsx'
-import LoginPasswordReset from './LoginPasswordReset.jsx'
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate.jsx';
+import LoginPasswordLost from './LoginPasswordLost.jsx';
+import LoginPasswordReset from './LoginPasswordReset.jsx';
+import { UserContext } from '../../UserContext.jsx';
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LoginForm/>} />
-        <Route path="criar" element={<LoginCreate/>} />
-        <Route path="perdeu" element={<LoginPasswordLost/>} />
-        <Route path="resetar" element={<LoginPasswordReset/>} />
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
