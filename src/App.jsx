015962decffdbe343dde0c367d64da5e47d11c6a
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-10-31 12:19:59 -0300

    rota da conta protegida apenas para quando o usuario estiver logado junto com um componente de protectedRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectRouter from './Components/Helper/ProtectRouter';
 
 function App() {
   return (
@@ -15,7 +17,13 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*"
+              element={
+                <ProtectRouter>
+                  <User />
+                </ProtectRouter>
+              }/>
           </Routes>
           <Footer />
         </UserStorage>
