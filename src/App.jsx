aacaa8c798f6f2e0be6d4c7e979db607519f5cff
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-11-13 22:33:12 -0300

    funcao laze criada para chamar os graficos so quando presisar e funcao de recovery de senha criada

--- src/App.jsx
+++ src/App.jsx
@@ -14,27 +14,29 @@ import NotFound from './Components/NotFound';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <Route path="conta/*"
-              element={
-                <ProtectRouter>
-                  <User />
-                </ProtectRouter>
-              }/>
-            <Route path='foto/:id' element={<Photo />}/>
-            <Route path='perfil/:user' element={<UserProfile />}/>
-            <Route path='*' element={<NotFound />}/>
-          </Routes>
+          <main className='AppBody'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route path="conta/*"
+                element={
+                  <ProtectRouter>
+                    <User />
+                  </ProtectRouter>
+                }/>
+              <Route path='foto/:id' element={<Photo />}/>
+              <Route path='perfil/:user' element={<UserProfile />}/>
+              <Route path='*' element={<NotFound />}/>
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { PASSWORD_RESET } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import { Navigate, useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const LoginPasswordReset = () => {
-  return (
-    <div>LoginPasswordReset</div>
-  )
-}
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate  = useNavigate();
 
-export default LoginPasswordReset
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate('/login');
+    }
+  }
+
+  return (
+    <section>
+      <Head title='Resete a senha' description='pagina de definição de nova senha'/>
+      <h1 className="title">Resete a senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+        <Error error={error} />
+      </form>
+    </section>
+  );
+};
+
+export default LoginPasswordReset;
